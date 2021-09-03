40c7365e631704e534b34847abc0e4156f279431
Ashik Chapagain <projectashik@gmail.com>
2021-09-03 18:49:47 +0545

    Vercel Check

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+import router from 'next/router';
+import { BaseSyntheticEvent, FormEvent, useState } from 'react';
 import { Header } from '../components/Header';
 
 const Home: NextPage = () => {
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState('');
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    setError('');
+    e.preventDefault();
+    if (!username) {
+      setError('Username is required');
+      return false;
+    }
+    router.push(`/card/${username}`);
+  };
   return (
     <div>
       <Head>
@@ -17,14 +30,26 @@ const Home: NextPage = () => {
           Generate awesome personalized cards which you can showcase to the
           community that contains all your awesome achievements on Hashnode
         </p>
-        <form className='flex flex-col items-center my-4 shadow-lg border gap-2 rounded p-4 w-full max-w-lg'>
+        <form
+          onSubmit={onSubmit}
+          className='flex flex-col items-center my-4 shadow-lg border gap-2 rounded p-4 w-full max-w-lg'
+        >
           <label htmlFor='username'>Hashnode Username</label>
           <input
             type='text'
-            className='border rounded block w-full p-3'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className={
+              'border rounded block w-full p-3 ' + (error && 'border-red-500 ')
+            }
             placeholder='Enter you hashnode username'
           />
-          <button className='flex gap-2 hover:bg-blue-700 items-center w-full justify-center bg-brand text-white py-3 rounded'>
+          <p className='self-start text-sm text-red-500'>{error && error}</p>
+          <button
+            className={
+              'flex gap-2 hover:bg-blue-700 items-center w-full justify-center bg-brand text-white py-3 rounded'
+            }
+          >
             <span className='text'>Generate Hashnode Card</span>
             <svg
               xmlns='http://www.w3.org/2000/svg'
@@ -42,6 +67,12 @@ const Home: NextPage = () => {
           </button>
         </form>
       </main>
+      <footer className='flex justify-center'>
+        Made with <span className='text-red-500 px-1'>&hearts;</span> by
+        <a href='https://github.com/projectashik'>&nbsp;Ashik Chapagain</a>
+        &nbsp;and
+        <a href='https://github.com/lalit2005'>&nbsp;Lalit Kishore</a>
+      </footer>
     </div>
   );
 };
