56e2154e3e4a37e3857aee5cab37a06fed31f322
Lalit <lalitvijay9480@gmail.com>
2021-09-07 11:56:19 +0530

    feat: add prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,15 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { Toaster } from "react-hot-toast";
-import Head from "next/head";
-import Script from "next/script";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <script
-          async
-          src="https://feedlr.vercel.app/init.js"
-          data-feedlr-project-id="f640418e-6b3d-46e7-87f8-1d507b80a373"></script>
-      </Head>
       <Script
-        src="https://feedlr.vercel.app/init.js"
-        data-feedlr-project-id="f640418e-6b3d-46e7-87f8-1d507b80a373"
-        strategy="lazyOnload"
+        src='https://feedlr.vercel.app/init.js'
+        data-feedlr-project-id='f640418e-6b3d-46e7-87f8-1d507b80a373'
+        strategy='lazyOnload'
       />
       <Component {...pageProps} />
       <Toaster />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap'
+            rel='stylesheet'
           />
         </Head>
         <body>
